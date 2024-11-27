68576e1c5dd45301985f5ffb4e364ea0659c3fc1
Queite <queitesc@gmail.com>
2024-11-27 13:23:31 -0300

    refactor: :fire: remove reslurcer creation with boto3

--- resources/create_s3.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import boto3
-import os
-
-load_dotenv()
-
-s3_client = boto3.client('s3')
-s3_client.create_bucket(Bucket=os.getenv("S3_BUCKET_NAME"))
--- resources/create_sns.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-sns = boto3.resource('sns')
-topic = sns.create_topic(Name="files_to_s3")
-
-subscription = topic.subscribe(
-  Protocol="sqs", Endpoint="arn:aws:sqs:us-east-1:123456789012:my-queue", ReturnSubscriptionArn=True
-)
