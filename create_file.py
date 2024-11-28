b3b7ff0a8d0cc7cdcae913c1cf712573f56fd209
Queite <queitesc@gmail.com>
2024-11-28 15:35:25 -0300

    refactor: :recycle: add num_lines to app

--- create_file.py
+++ create_file.py
@@ -1,3 +1,4 @@
+import json
 import random
 from datetime import datetime
 import os
@@ -26,20 +27,24 @@ def generate_file():
     upload_to_s3(file_name, file_content.getvalue())
 
 
-def upload_to_s3(file_name, file_content):
+def upload_to_s3(file_name, file_content, num_lines):
     s3 = boto3.client('s3')
     bucket_name = os.getenv('S3_BUCKET_NAME')
     s3.put_object(Bucket=bucket_name, Key=file_name, Body=file_content)
     print(f"Uploaded {file_name} to S3 using boto3")
-    send_sns_notification(file_name)
+    send_sns_notification(file_name, num_lines)
 
 
-def send_sns_notification(file_name):
+def send_sns_notification(file_name, num_lines):
     sns = boto3.client('sns')
     topic_arn = os.getenv('SNS_TOPIC_ARN')
-    message = f"File {file_name} has been uploaded to S3."
+    message = json.dumps({
+        "file_name": file_name,
+        "num_lines": num_lines,
+        "message": f"File {file_name} has been uploaded to S3."
+    })
     sns.publish(TopicArn=topic_arn, Message=message)
-    print(f"Sent SNS notification for {file_name}")
+    print(f"Sent SNS notification for {file_name} with {num_lines} lines")
 
 
 if __name__ == "__main__":
