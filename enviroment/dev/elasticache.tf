a5aa6aafd0891f3946200578df57ecc778ee2405
Queite <queitesc@gmail.com>
2024-11-28 15:18:29 -0300

    refactor(aws): :fire: removes unnecessary network config

--- enviroment/dev/elasticache.tf
+++ enviroment/dev/elasticache.tf
@@ -8,29 +8,3 @@ resource "aws_elasticache_cluster" "contabil" {
   security_group_ids = [aws_security_group.elasticache_sg.id]
   subnet_group_name  = aws_elasticache_subnet_group.elasticache_subnet_group.name
 }
-
-resource "aws_security_group" "elasticache_sg" {
-  name        = "elasticache_sg"
-  description = "Allow access to Elasticache cluster"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 6379
-    to_port     = 6379
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_elasticache_subnet_group" "elasticache_subnet_group" {
-  name       = "elasticache_subnet_group"
-  subnet_ids = var.subnet_ids
-  description = "Elasticache subnet group"
-}
