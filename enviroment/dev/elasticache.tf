9a935af13d2d50d4c35f25a36166383360f69612
Queite <queitesc@gmail.com>
2024-11-28 16:14:26 -0300

    fix: :bug: fixes problems point out by terraform plan

--- enviroment/dev/elasticache.tf
+++ enviroment/dev/elasticache.tf
@@ -2,7 +2,7 @@ resource "aws_elasticache_cluster" "contabil" {
   cluster_id           = var.elasticache_cluster_id
   engine               = var.elasticache_engine
   node_type            = var.elasticache_node_type
-  num_cache_nodes      = 2
+  num_cache_nodes      = 1
   parameter_group_name = var.elasticache_parameter_group_name
   port                 = 6379
 }
--- enviroment/dev/lambda.tf
+++ enviroment/dev/lambda.tf
@@ -1,15 +1,10 @@
 resource "aws_lambda_function" "process_s3_file" {
-  filename         = "lambda_function_payload.zip"
+  filename         = data.archive_file.lambda.output_path
   function_name    = "process_s3_file"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "lambda_function.lambda_handler"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  source_code_hash = filebase64sha256(data.archive_file.lambda.output_path)
   runtime          = "python3.8"
-  environment {
-    variables = {
-      S3_BUCKET_NAME = var.bucket_name
-    }
-  }
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
@@ -25,5 +20,5 @@ resource "aws_iam_policy" "allow_sqs_elasticache_policy" {
 
 resource "aws_iam_role_policy_attachment" "allow_sqs_elasticache_policy_attach" {
   role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.allow_sqs_elasticache_policy
+  policy_arn = aws_iam_policy.allow_sqs_elasticache_policy.arn
 }
