b0d2cfd69d512d4e76fb74a33f5c0eee8e917757
Queite <queitesc@gmail.com>
2024-11-28 15:27:00 -0300

    refactor(aws): :recycle: fix problems finded in terraform plan

--- enviroment/dev/elasticache.tf
+++ enviroment/dev/elasticache.tf
@@ -2,9 +2,7 @@ resource "aws_elasticache_cluster" "contabil" {
   cluster_id           = var.elasticache_cluster_id
   engine               = var.elasticache_engine
   node_type            = var.elasticache_node_type
-  num_cache_nodes      = var.elasticache_num_cache_nodes
+  num_cache_nodes      = 2
   parameter_group_name = var.elasticache_parameter_group_name
-
-  security_group_ids = [aws_security_group.elasticache_sg.id]
-  subnet_group_name  = aws_elasticache_subnet_group.elasticache_subnet_group.name
+  port                 = 111211
 }
