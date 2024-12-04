2548d179603d98425f14a3b73068c760385a8e3f
Queite <queitesc@gmail.com>
2024-12-04 10:42:16 -0300

    feat: :heavy_plus_sign: add redis layer to labda

--- enviroment/dev/lambda-sqs-elasticache.tf
+++ enviroment/dev/lambda-sqs-elasticache.tf
@@ -1,6 +1,27 @@
+resource "aws_lambda_layer_version" "redis_layer" {
+  filename   = "redis.zip"
+  layer_name = "redis"
+
+  compatible_runtimes = ["python3.8"]
+}
+
+data "aws_iam_policy_document" "sqs_elasticache_lambda_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
 resource "aws_iam_role" "iam_for_lambda_sqs_elasticache" {
   name = "iam_for_lambda_sqs_elasticache"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.sqs_elasticache_lambda_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "sqs_full_access_attach" {
@@ -15,37 +36,36 @@ resource "aws_iam_role_policy_attachment" "elasticache_full_access_attach" {
 
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
   event_source_arn = aws_sqs_queue.new_object_queue.arn
-  function_name    = aws_lambda_function.process_sqs_message.arn
+  function_name    = aws_lambda_function.save-on-elasticache.arn
   batch_size       = 10
   enabled          = true
 }
 
-resource "aws_lambda_function" "process_sqs_message" {
+resource "aws_lambda_function" "save-on-elasticache" {
   filename         = data.archive_file.lambda_sqs_elasticache.output_path
-  function_name    = "process_sqs_message"
+  function_name    = "save-on-elasticache"
   role             = aws_iam_role.iam_for_lambda_sqs_elasticache.arn
   handler          = "lambda_sqs_elasticache.lambda_handler"
   source_code_hash = filebase64sha256(data.archive_file.lambda_sqs_elasticache.output_path)
   runtime          = "python3.8"
+
+  layers           = [aws_lambda_layer_version.redis_layer.arn]
 
   environment {
     variables = {
       ELASTICACHE_ENDPOINT = aws_elasticache_cluster.contabil.cache_nodes[0].address
     }
   }
+
+  depends_on = [ aws_lambda_layer_version.redis_layer ]
 }
 
 resource "aws_lambda_permission" "allow_sqs" {
   statement_id  = "AllowExecutionFromSQS"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.process_sqs_message.arn
+  function_name = aws_lambda_function.save-on-elasticache.arn
   principal     = "sqs.amazonaws.com"
   source_arn    = aws_sqs_queue.new_object_queue.arn
-}
-
-resource "aws_sqs_queue_policy" "allow_lambda_sqs_policy" {
-  queue_url = aws_sqs_queue.new_object_queue.id
-  policy    = data.aws_iam_policy_document.sqs_sns_policy.json
 }
 
 data "archive_file" "lambda_sqs_elasticache" {
