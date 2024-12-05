d610a380dd1389de340ceca9f60409bc02d9bbda
Queite <queitesc@gmail.com>
2024-12-05 10:15:50 -0300

    refactor: :recycle: changes elasticache to rds

--- enviroment/dev/elasticache.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_elasticache_cluster" "contabil" {
-  cluster_id           = var.elasticache_cluster_id
-  engine               = var.elasticache_engine
-  node_type            = var.elasticache_node_type
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis7"
-  port                 = 6379
-}
--- enviroment/dev/main.tf
+++ enviroment/dev/main.tf
@@ -17,3 +17,37 @@ provider "aws" {
     }
   }
 }
+
+resource "aws_security_group" "db_sg" {
+  name        = "db_sg"
+  vpc_id      = aws_vpc.contabil_vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc" "contabil_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "db_subnet_1" {
+  vpc_id            = aws_vpc.contabil_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "db_subnet_2" {
+  vpc_id            = aws_vpc.contabil_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
